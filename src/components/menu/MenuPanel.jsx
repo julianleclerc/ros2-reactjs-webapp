0b321e11339d69270804c355baec7c9988743394
Fyier <121387665+julianleclerc@users.noreply.github.com>
2025-03-11 16:09:59 +0200

    logo

--- src/components/menu/MenuPanel.jsx
+++ src/components/menu/MenuPanel.jsx
@@ -1,27 +1,38 @@
 import React from "react";
 import "./MenuPanel.css";
+import temoto_logo from "../../public/temoto_logo_icon.png";
 
 const MenuPanel = ({ setPage }) => {
-    // Define button configurations
-    const buttons = [
-        { id: 1, text: "Chat", page: "ChatInterfacePage" },
-        { id: 2, text: "Action", page: "ActionInterfacePage" },
-    ];
+  const buttons = [
+    { id: 1, text: "Chat", page: "ChatInterfacePage" },
+    { id: 2, text: "Action", page: "ActionInterfacePage" },
+  ];
 
-    // Render the menu panel with buttons
-    return (
-        <div className="menu-panel">
-            {buttons.map((button) => (
-                <button
-                    key={button.id}
-                    className="menu-button"
-                    onClick={() => setPage(button.page)}
-                >
-                    {button.text} 
-                </button>
-            ))}
-        </div>
-    );
+  const info = { text: "Info", page: "InfoInterfacePage" };
+
+  return (
+    <div className="menu-panel">
+      {buttons.map((button) => (
+        <button
+          key={button.id}
+          className="menu-button"
+          onClick={() => setPage(button.page)}
+        >
+          {button.text}
+        </button>
+      ))}
+      
+      {/* New bottom container */}
+      <div className="menu-panel-bottom">
+        <button
+          className="info-button"
+          onClick={() => setPage(info.page)}
+        >
+          {<img src={temoto_logo} alt="Temoto Logo" className="temoto-logo" />}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MenuPanel;
